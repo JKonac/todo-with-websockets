4c9995e09c6cd225960bbf42fb2a015787e13ff7
JKonac <joakimkonac@gmail.com>
2023-11-03 18:45:16 +0100

    Fix share URL link

--- src/components/TodoListContainer/components/TodoListSideBar/index.tsx
+++ src/components/TodoListContainer/components/TodoListSideBar/index.tsx
@@ -8,15 +8,23 @@ export default function TodoListSideBar({
   selectedTable: { listID: string; name: string };
 }) {
   const shareHandler = () => {
-    navigator.clipboard.writeText(
-      `localhost:3000/?listID=${selectedTable.listID}`
-    );
+    navigator.clipboard.writeText(location.host + location.pathname + "?listID=" + selectedTable.listID);
   };
   return (
     <div className="min-w-[260px] border-r-[1px] mr-5 h-[300px] truncate">
       <p className="font-medium text-2xl">{todoListTitle}</p>
-      <button onClick={() => shareHandler()} className="flex items-center border border-gray-300 hover:bg-gray-100 rounded p-2 text-sm mt-4">
-        <Image src={"share_icon.svg"} alt="share" width={20} height={20} className="mr-2" /> Share list
+      <button
+        onClick={() => shareHandler()}
+        className="flex items-center border border-gray-300 hover:bg-gray-100 rounded p-2 text-sm mt-4"
+      >
+        <Image
+          src={"share_icon.svg"}
+          alt="share"
+          width={20}
+          height={20}
+          className="mr-2"
+        />{" "}
+        Share list
       </button>
     </div>
   );
