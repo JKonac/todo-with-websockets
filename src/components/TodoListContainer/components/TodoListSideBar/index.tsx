f00ac44651f160373ea6f8ca33b4d132a1228a9c
JKonac <joakimkonac@gmail.com>
2023-11-03 18:38:56 +0100

    Fix sizing and memoize functions

--- src/components/TodoListContainer/components/TodoListSideBar/index.tsx
+++ src/components/TodoListContainer/components/TodoListSideBar/index.tsx
@@ -13,7 +13,7 @@ export default function TodoListSideBar({
     );
   };
   return (
-    <div className="min-w-[260px] border-r-[1px] mr-5 h-[300px]">
+    <div className="min-w-[260px] border-r-[1px] mr-5 h-[300px] truncate">
       <p className="font-medium text-2xl">{todoListTitle}</p>
       <button onClick={() => shareHandler()} className="flex items-center border border-gray-300 hover:bg-gray-100 rounded p-2 text-sm mt-4">
         <Image src={"share_icon.svg"} alt="share" width={20} height={20} className="mr-2" /> Share list
