6e36587c7d69c7ccf159b695bb3aa00d3e81d488
JKonac <joakimkonac@gmail.com>
2023-11-06 09:51:14 +0100

    Update type casting to already declared interface

--- src/components/TodoListContainer/components/ActiveUsers/index.tsx
+++ src/components/TodoListContainer/components/ActiveUsers/index.tsx
@@ -10,7 +10,7 @@ interface ActiveUsersProps {
 export default function ActiveUsers({ activeUsers }: ActiveUsersProps) {
   return (
     <div className="h-10 flex mb-4">
-      {activeUsers?.map((user: { userId: string; userName: string }) => (
+      {activeUsers?.map((user: User) => (
         <div className="w-7 h-7 flex justify-center items-center uppercase text-xs rounded-full bg-emerald-600 text-white mr-2">
           {user.userName}
         </div>
